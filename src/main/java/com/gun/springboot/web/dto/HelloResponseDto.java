ab0fa670d078eda7c86a6b47bef67a595c72b7af
박이건 <parkegun@gmail.com>
2020-12-19 21:01:16 +0900

    2장 - 테스트

---
+++ src/main/java/com/gun/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.gun.springboot.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
