0f9091f16bde98266f9ecce76674132106739edb
박이건 <parkegun@gmail.com>
2020-12-20 20:40:03 +0900

    3장 JPA 다루기

---
+++ src/main/java/com/gun/springboot/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.gun.springboot.web;
+
+import com.gun.springboot.service.PostsService;
+import com.gun.springboot.web.dto.PostsResponseDto;
+import com.gun.springboot.web.dto.PostsSaveRequestDto;
+import com.gun.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PostMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
